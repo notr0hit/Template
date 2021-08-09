bf3d00fdf1625e8025302ea82e67555d63d0cb1e
Rohit Khare <76691776+notr0hit@users.noreply.github.com>
2021-08-10 02:13:42 +0530

    Create coordinate_compression.cpp

---
+++ coordinate_compression.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+
+    for(int i = 0; i<n; i++) {
+      cin >> A[i];
+    }
+    
+    set<int> values;
+
+    for(int i = 0; i<n; i++) {
+      values.insert(A[i]);
+    }
+
+    map<int, int> comp;
+
+    for(int val : values) {
+      comp[val] = comp.size();
+    }
+
+    //Now Array *A* is compressed to *comp* 
+    // relative position of Array can be accesed using index of A[i]
+
+}
